dacccc740efec38a4ec8aeff1e5c2b497caa4779
Viet Le <lethaiviet92@gmail.com>
2022-07-01 16:50:47 +0700

    * using TokenService instead verify token with secret

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,17 +1,15 @@
 import { NextFunction, Response } from 'express';
-import { verify } from 'jsonwebtoken';
-import { SECRET_KEY } from '@config';
 import { HttpException } from '@exceptions/HttpException';
-import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
+import { RequestWithUser } from '@interfaces/auth.interface';
 import userModel from '@models/users.model';
+import TokenService from '@/services/token.service';
 
 const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
     const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
 
     if (Authorization) {
-      const secretKey: string = SECRET_KEY;
-      const verificationResponse = verify(Authorization, secretKey) as DataStoredInToken;
+      const verificationResponse = TokenService.verifyAndGetDataInToken(Authorization);
       const userId = verificationResponse._id;
       const findUser = await userModel.findById(userId);
 
